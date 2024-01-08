7bc208908729b3864e20f45eaa3816f31cbf8476
Phat Tran <ptran@strongtie.com>
2024-01-08 19:52:35 +0700

    Initiatlize HoneyVault class

---
+++ BeehiveManagementSystem/HoneyVault.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    static class HoneyVault
+    {
+        public static string StatusReport { get; }
+
+        private static float honey = 25f;
+        private static float nectar = 100f;
+
+        public static void CollectNectar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ConvertNectarToHoney()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool ConsumeHoney()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
