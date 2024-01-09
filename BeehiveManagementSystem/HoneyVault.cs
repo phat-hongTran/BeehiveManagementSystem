606d2dfad37fd212441a6b99fd2da5315fcd5b3c
Phat Tran <ptran@strongtie.com>
2024-01-09 07:43:19 +0700

    Finish HoneyVault

--- BeehiveManagementSystem/HoneyVault.cs
+++ BeehiveManagementSystem/HoneyVault.cs
@@ -8,24 +8,62 @@ namespace BeehiveManagementSystem
 {
     static class HoneyVault
     {
-        public static string StatusReport { get; }
+        public static string StatusReport 
+        { 
+            get 
+            {
+                string report = "";
+                report += "Vault report:";
+                report += "\n";
+                report += $"{honey} units of honey";
+                report += "\n";
+                report += $"{nectar} units of nectar";
+                if (honey < LOW_LEVEL_WARNING)
+                {
+                    report += "\n";
+                    report += "LOW HONEY - ADD A HONEY MANUFACTURER";
+                }
+                report += "\n";
+                return report;
+            } 
+        }
 
         private static float honey = 25f;
         private static float nectar = 100f;
 
-        public static void CollectNectar()
+        private const float NECTAR_CONVERSION_RATIO = .19f;
+        private const float LOW_LEVEL_WARNING = 10f;
+
+        public static void CollectNectar(float amount)
         {
-            throw new NotImplementedException();
+            if (amount > 0f)
+            {
+                nectar += amount;
+            }
         }
 
-        public static void ConvertNectarToHoney()
+        public static void ConvertNectarToHoney(float amount)
         {
-            throw new NotImplementedException();
+            if (amount > nectar)
+            {
+                amount = nectar;
+            }
+
+            nectar -= amount;
+            honey += amount * NECTAR_CONVERSION_RATIO;
         }
 
-        public static bool ConsumeHoney()
+        public static bool ConsumeHoney(float amount)
         {
-            throw new NotImplementedException();
+            if (amount > honey)
+            {
+                return false;
+            }
+            else
+            {
+                honey -= amount;
+                return true;
+            }
         }
     }
 }
